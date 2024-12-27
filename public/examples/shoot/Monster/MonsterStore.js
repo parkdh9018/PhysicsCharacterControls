24888fc1db738b42ff97ccabc04c0989dd4c12e7
Jian Nam <jeeahn2929@gmail.com>
2024-12-27 10:29:42 +0900

    refactor: remove unnecessary object array from MonsterStore

--- public/examples/shoot/Monster/MonsterStore.js
+++ public/examples/shoot/Monster/MonsterStore.js
@@ -2,18 +2,15 @@ export class MonsterStore {
   constructor() {
     this.length = 0;
     this._monsters = [];
-    this._objects = [];
   }
   add(monster) {
     this._monsters.push(monster);
-    this._objects.push(monster.object);
     this.length = this._monsters.length;
   }
   delete(monster) {
     const monsterIndex = this._monsters.indexOf(monster);
     if (monsterIndex == -1) return;
     this._monsters.splice(monsterIndex, 1);
-    this._objects.splice(monsterIndex, 1);
   }
   findMonsterByObject(object) {
     return this._monsters.find(monster => monster.object.getObjectById(object.id));
@@ -24,6 +21,6 @@ export class MonsterStore {
   }
 
   get objects() {
-    return this._objects;
+    return this._monsters.map(monster => monster.object);
   }
 }
