98206c396efad3ab138e14ea9a724002d5e7f31f
Byongho Lee <47059013+Byongho96@users.noreply.github.com>
2025-01-03 15:29:13 +0900

    build: change rollup-plugin-terser to rollup/plugin-terer (#25)
    - rollup-plugin-terser has been deprecated

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
